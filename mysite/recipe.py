cbf87bcebaa3dfa8808c84b2f7113d3b2650b982
Kevin Weston <kcweston1@cougars.ccis.edu>
2018-02-26 16:29:17 -0600

    initial commit

---
+++ mysite/recipe.py
@@ -0,0 +1,26 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '''
+
+    Hello from Flask!
+    '''
+
+
+@app.route('/login')
+def login():
+    return '''
+
+<html>
+
+
+
+</html>
+
+    '''

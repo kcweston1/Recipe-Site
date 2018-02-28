f0a5c3e1e4eb1968d7d23ea439a26598ceeb05ad
bhattaway <bhattaway98@gmail.com>
2018-02-28 16:29:48 -0600

    login page: removed cancel button, made method POST (instead of the default GET)

--- mysite/recipe.py
+++ mysite/recipe.py
@@ -32,22 +32,16 @@ def login():
 
 		<p>This is the login page.</p>
 
-		<form action="/">
+		<form action="/" method="post">
+			<div class="container">
+				<label for="uname"><b>Username</b></label>
+				<input type="text" placeholder="Enter Username" name="uname" required>
 
-		<div class="container">
-			<label for="uname"><b>Username</b></label>
-			<input type="text" placeholder="Enter Username" name="uname" required>
+				<label for="psw"><b>Password</b></label>
+				<input type="password" placeholder="Enter Password" name="psw" required>
 
-			<label for="psw"><b>Password</b></label>
-			<input type="password" placeholder="Enter Password" name="psw" required>
-
-			<button type="submit">Login</button>
-		  </div>
-
-		  <div class="container" style="background-color:#f1f1f1">
-			<button type="button" class="cancelbtn">Cancel</button>
-			<span class="psw">Forgot <a href="#">password?</a></span>
-		  </div>
+				<button type="submit">Login</button>
+			</div>
 		</form>
 	</body>
 

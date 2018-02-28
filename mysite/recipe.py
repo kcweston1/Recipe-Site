6f7b5385073dc85edda718841110be3463339142
bhattaway <bhattaway98@gmail.com>
2018-02-28 16:19:19 -0600

    added basic login page

--- mysite/recipe.py
+++ mysite/recipe.py
@@ -26,6 +26,30 @@ def login():
     return '''
 
 <html>
+	<title>recipe0 login page</title>
+	<body>
+		<h1>Login to recipe0!</h1>
+
+		<p>This is the login page.</p>
+
+		<form action="/">
+
+		<div class="container">
+			<label for="uname"><b>Username</b></label>
+			<input type="text" placeholder="Enter Username" name="uname" required>
+
+			<label for="psw"><b>Password</b></label>
+			<input type="password" placeholder="Enter Password" name="psw" required>
+
+			<button type="submit">Login</button>
+		  </div>
+
+		  <div class="container" style="background-color:#f1f1f1">
+			<button type="button" class="cancelbtn">Cancel</button>
+			<span class="psw">Forgot <a href="#">password?</a></span>
+		  </div>
+		</form>
+	</body>
 
 
 

f742e22de985afc43d10a225c7ef93d6ab46716e
Kevin Weston <kcweston1@cougars.ccis.edu>
2018-02-28 16:31:01 -0600

    Added recipe submission page

--- mysite/recipe.py
+++ mysite/recipe.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    
     return '''
-
-    Hello from Flask!
+    <a href = '/login'>Log in</a>
     '''
 
 
@@ -23,4 +23,21 @@ def login():
 
 </html>
 
+    '''
+
+@app.route('/submitRecipe')
+def submit():
+    return '''
+    <h1>Submit a new recipe</h1>
+    <form action='completeSubmission' method='post'>
+        Recipe name<br> <input type='text' name='recipeName'></input>
+        Prep time<br> <input type='text' name='prepTime'></input>
+        Cook time<br> <input type='text' name='cookTime'></input>
+        Ingredients<br> <input type='text' name='ingredients'></input>
+        Instructions<br> <textarea rows=4 cols=50 name='instructions'></textarea>
+        Ingredients<br> <input type='text' name='ingredients'></input>
+        Description<br> <textarea rows=4 cols=50 name='description'></textarea>
+        Category<br> <input type='text' name='category'></input>
+        <input type='submit' name='submit'></input>
+    </form>
     '''

2c682ac12c192f9e87ca534fc4ebced3b5bac76f
kcweston1 <kcweston1@cougars.ccis.edu>
2018-03-10 01:04:05 +0000

    Added query to created unconfirmed users table.

--- mysite/db_init.sql
+++ mysite/db_init.sql
@@ -2,7 +2,15 @@ create table users(
     id int,
     passwordhash text,
     email varchar(200),
-    username varchar(200),
+    username varchar(30),
     salt char(32),
     primary key (id)
     ) engine=innodb;
+
+create table unconfirmed_users(
+    email varchar(200),
+    username varchar(30),
+    passwordhash text,
+    salt char(32),
+    confirmationcode char(32)
+    );

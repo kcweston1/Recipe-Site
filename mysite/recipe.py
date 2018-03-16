aad0e440845167e2f56e514e4c58416f25846935
kcweston1 <kcweston1@cougars.ccis.edu>
2018-03-16 22:40:33 +0000

    Added templating

--- mysite/recipe.py
+++ mysite/recipe.py
@@ -1,64 +1,23 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, request, render_template, session
-import MySQLdb, string, hashlib, secrets, functions, mail
+from flask import Flask, request, render_template, session, escape, url_for, redirect, flash
+import MySQLdb, string, hashlib, secrets, functions, mail, datetime
 
 app = Flask(__name__)
 
 # generate random string for the session's secret key
-app.secret_key = 'randomstring'
+app.secret_key = b"D>',\xf1\xa0\xdf\x16i\x96\xe5y\xe9\x91@\xf7\x95\xad\xd9P\xbb%\xe9\r"
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-    usernametext = ''
-    loginout = ''
-    if 'username' in session:
-        sessionusername = session['username']
-        usernametext = 'Logged in as ' + sessionusername + '.'
-        loginout = "<a href = '/logout'>Log out</a></br>"
-    else:
-        usernametext = 'Not logged in'
-        loginout = "<a href = '/login'>Log in</a></br>"
-
-    #check if email is in db
-    #if yes, check to see that password + user.salt = hashedpassword
-    #  by running password + user.salt through hash
-    #if no, flash message for invalid login.
-
-    return '''
-    <html>
-		<title>recipe0 home page</title>
-		<body>
-            <p>%s</p>
-			<h1>Welcome to recipe0!</h1>
-
-			<p>This is the home page.</p>
-            %s
-            <a href = '/submitrecipe'>Submit a recipe</a></br>
-            <a href='/signup'>Sign up</a></br>
-		</body>
-	</html>
-    ''' % (usernametext, loginout)
+    return render_template('home.html')
 
 
 @app.route('/signup')
 def signup():
-    return '''
-<html>
-    <title>recipe0 account creator</title>
-    <body>
-        <h1>Create an account on recipe0</h1>
-        <form action = '/signupstatus' method='post'>
-            Email <input type='text' name='email' required/>
-            Username <input type='text' name='uname' required />
-            Password <input type='password' name='psw' required />
-            <button type='submit' name='signup'>Create account</button>
-        </form>
-    </body>
-</html>
-    '''
+    return render_template('signup.html')
 
 @app.route('/signupstatus', methods=['POST'])
 def signup_status():
@@ -68,18 +27,21 @@ def signup_status():
     email = request.form.get('email').lower()
     username = request.form.get('uname')
     password = request.form.get('psw')
+    confirm_password = request.form.get('confirmpsw')
+
+    if password != confirm_password:
+        flash("Passwords did not match")
+        return redirect(url_for('signup'))
+
     cursor.execute('select * from users')
     users = cursor.fetchall()
 
     if (len(users) > 0):
         for user in users:
             if (user['email'].lower() == email.lower() or user['username'] == username):
-                return '''
-                <html>
-                    Email/username already in use!
-                    <a href='/signup'>Return to signup</a>
-                </html>
-                '''
+                flash("Email/username already in use.")
+                return redirect(url_for('signup'))
+
     #hash the current password here
     salt = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(32))
     salted_pass = password + salt
@@ -94,48 +56,20 @@ def signup_status():
     conn.close()
 
     functions.send_confirmation_email(email, confirmation_code)
+    flash("Account creation successful!")
 
-    return '''
-    <html>
-        Account creation successful!
-        <a href='/login'>Click here to log in</a>
-    </html>
-    '''
+    return redirect(url_for('home'))
 
 
 @app.route('/login')
 def login():
-    return '''
+    return render_template('login.html')
 
-<html>
-	<title>recipe0 login page</title>
-	<body>
-		<h1>Login to recipe0!</h1>
-
-		<p>This is the login page.</p>
-
-		<form action="/loggedin" method="post">
-			<label for="uname"><b>Username</b></label>
-			<input type="text" placeholder="Enter Username" name="uname" required />
-
-			<label for="psw"><b>Password</b></label>
-			<input type="password" placeholder="Enter Password" name="psw" required />
-
-			<button type="submit" name='login'>Login</button>
-		</form>
-	</body>
-
-
-
-</html>
-
-    '''
-
-@app.route('/loggedin', methods = ['GET', 'POST'])
+@app.route('/processlogin', methods = ['GET', 'POST'])
 def loggedin():
-    login_status = ''
+    redirect_to = redirect(url_for('login'))
     if request.method == 'POST':
-        session['username'] = request.form.get('uname')
+
 
         conn = functions.db_connect()
         cursor = conn.cursor(MySQLdb.cursors.DictCursor)
@@ -148,46 +82,109 @@ def loggedin():
         for user in users:
             if user['username'] == username:
                 if functions.check_password(user, password):
-                    login_status = "login successful!"
+                    session['username'] = escape(request.form.get('uname'))
+                    session['logged_in'] = True
+                    redirect_to = redirect(url_for('home'))
                 else:
-                    login_status = "login failed!"
+                    flash('Invalid password')
 
-    usernametext = ''
-    if 'username' in session:
-        sessionusername = session['username']
-        usernametext = 'Logged in as ' + sessionusername + '.'
+    return redirect_to
+
+
+@app.route('/forgotpassword')
+def forgotpassword():
+    return render_template('forgotpassword.html')
+
+@app.route('/processpasswordreset', methods = ['POST'])
+def process_password_reset():
+    conn = functions.db_connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+
+    username = request.form.get('username')
+    reset_code = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(32))
+    email = None
+
+    cursor.execute("select * from users")
+    users = cursor.fetchall()
+
+    for user in users:
+        if username == user['username']:
+            email = user['email']
+            sql = 'update users set reset_code = "%s", reset_time = CURRENT_TIMESTAMP where username = "%s"' % (reset_code, username)
+            cursor.execute(sql)
+            functions.send_password_reset_email(email, reset_code)
+            conn.commit()
+            conn.close()
+
+            flash("Email sent!")
+            return redirect(url_for('home'))
+
+    conn.close()
+    flash("Username not found!")
+    return redirect(url_for('forgotpassword'))
+
+
+@app.route('/resetpassword', methods = ['GET'])
+def reset_password():
+    reset_code = request.args.get('code')
+
+    conn = functions.db_connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('select * from users where reset_code = "%s"' % (reset_code))
+    user = cursor.fetchall()
+    conn.close()
+
+    username = user[0]['username']
+    session['usernameforpwreset'] = username
+    session['codeforpwreset'] = reset_code
+
+    return render_template('resetpassword.html')
+
+
+@app.route('/finalizepasswordreset', methods=['GET', 'POST'])
+def finalizepasswordreset():
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirmpassword')
+    username = session['usernameforpwreset']
+    code = session['codeforpwreset']
+    session.pop('usernameforpwreset', None)
+    session.pop('codeforpwreset', None)
+
+    if password == confirm_password:
+        conn = functions.db_connect()
+        cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('select * from users where username = "%s"' % (username))
+        user = cursor.fetchall()
+        print(user)
+
+        if functions.longer_than_one_day(user[0]['reset_time']):
+            flash("Password reset has expired. Try again.")
+            cursor.execute('update users set reset_time = NULL, reset_code = NULL where username = "%s"' % (username))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('forgotpassword'))
+        elif user[0]['reset_code'] == code:
+            flash("Password reset successfully")
+            salt = ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(32))
+            hashed_pass = functions.hash_password(password, salt)
+            cursor.execute('update users set passwordhash = "%s", salt = "%s", reset_code = NULL, reset_time = NULL where username = "%s"' % (hashed_pass, salt, username))
+        else:
+            flash("Reset code did not match username")
+
+        conn.commit()
+        conn.close()
+        return redirect(url_for('home'))
     else:
-        usernametext = 'Not logged in'
+        flash("Passwords do not match.")
+        return redirect(url_for('resetpassword'))
 
-    return '''
-    <html>
-		<title>recipe0 home page</title>
-		<body>
-
-		    <p>%s</p>
-            <p>%s</p>
-			<h1>You landed on the post-login page!</h1>
-
-			<a href = '/'>Go to home page</a></br>
-            <a href = '/submitrecipe'>Submit a recipe</a></br>
-		</body>
-	</html>
-    ''' % (login_status, usernametext)
 
 @app.route('/logout')
 def logout():
 #remove the username from the session, if it is there.
     session.pop('username', None)
-    return '''
-    <html>
-        <title>Logout</title>
-        <body>
-            <h2>Logged out.</h2>
-            <a href = '/'>Go to home page</a></br>
-            <a href = '/login'>Log in</a></br>
-        </body>
-    </html>
-    '''
+    session['logged_in'] = False
+    return redirect(url_for('home'))
 
 @app.route('/submitrecipe')
 def submit():
@@ -206,56 +203,31 @@ def submit():
     </form>
     '''
 
-@app.route('/confirm', methods=["GET", "POST"])
+
+@app.route('/confirm', methods=['GET'])
 def confirm():
-    if request.method == "POST":
-        conn = functions.db_connect()
-        cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    conn = functions.db_connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
 
-        email = request.form.get('email').lower()
-        confirmation = request.form.get('confirmation')
-        cursor.execute('select * from unconfirmed_users')
-        users = cursor.fetchall()
+    cursor.execute('select * from unconfirmed_users')
+    users = cursor.fetchall()
 
-        for user in users:
-            if user["email"] == email:
-                if user["confirmationcode"] == confirmation:
-                    new_id = functions.get_new_id("users", conn, cursor)
+    for user in users:
+        if user["confirmationcode"] == request.args.get('code'):
+            new_id = functions.get_new_id("users", conn, cursor)
 
-                    sql = "insert users (email, username, passwordhash, salt, id) values ('%s', '%s', '%s', '%s', %d)" % (user["email"], user["username"], user["passwordhash"], user["salt"], new_id)
-                    cursor.execute(sql)
-                    sql = "delete from unconfirmed_users where email = \"%s\"" % (user["email"])
-                    cursor.execute(sql)
-                    conn.commit()
-                    conn.close()
-                    return '''
-                    success
-                    '''
-        return '''
-        failed for some reason
-        '''
-
-    else:
-        return '''
-<html>
-    <h1>Confirm your account</h1>
-    <form action='confirm' method='post'>
-        email <input type='text' name='email' required />
-        Confirmation Code <input type='text' name='confirmation' required />
-        <button type='submit' name='submit'></button>
-    </form>
-</html>
-'''
-
-
-#@app.route('/testconfirm', methods=['GET'])
-#    conn = functions.db_connect()
-#    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
-
-#    cursor.execute('select * from unconfirmed_users')
-#        users = cursor.fetchall()
-
-
+            sql = "insert users (email, username, passwordhash, salt, id) values ('%s', '%s', '%s', '%s', %d)" % (user["email"], user["username"], user["passwordhash"], user["salt"], new_id)
+            cursor.execute(sql)
+            sql = "delete from unconfirmed_users where email = \"%s\"" % (user["email"])
+            cursor.execute(sql)
+            conn.commit()
+            conn.close()
+            return '''
+            success
+            '''
+    return '''
+    failed for some reason
+    '''
 
 
 

8af9050d11202bed58a6bec9e9c2968a3cea3bea
kcweston1 <kcweston1@cougars.ccis.edu>
2018-03-16 22:41:34 +0000

    Added 'unconfirmed_users'

--- mysite/db_init.sql
+++ mysite/db_init.sql
@@ -1,9 +1,11 @@
 create table users(
     id int,
-    passwordhash text,
     email varchar(200),
     username varchar(30),
+    passwordhash text,
     salt char(32),
+    reset_time timestamp,
+    reset_code char(32),
     primary key (id)
     ) engine=innodb;
 
